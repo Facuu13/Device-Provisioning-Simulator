c214dca5925a0be527fcf7391791223cf4ac5bc5
Facuu13 <facundoo30@gmail.com>
2026-01-20 15:48:42 -0300

    creamos, insertamos y devuelve la listas de dispositivos

--- db.py
+++ db.py
@@ -24,3 +24,30 @@ def init_db():
     conn.commit()
     conn.close()
 
+def insert_device(device: dict):
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO devices (device_id, state, created_at, provisioned_at, provision_token, activated_at, fw_version)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    ''', (
+        device['device_id'],
+        device['state'],
+        device['created_at'],
+        device['provisioned_at'],
+        device['provision_token'],
+        device['activated_at'],
+        device['fw_version']
+    ))
+    conn.commit()
+    conn.close()
+
+def list_devices():
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM devices')
+    rows = cursor.fetchall()
+    devices = [dict(row) for row in rows]
+    conn.close()
+    return devices
+
--- test_db.py
+++ test_db.py
@@ -1,3 +1,17 @@
-from db import init_db
+from db import init_db, insert_device, list_devices
 init_db()
 print("DB OK")
+
+device = {
+    "device_id": "dev001",
+    "state": "NEW",
+    "created_at": 1625247600,
+    "provisioned_at": None,
+    "provision_token": None,
+    "activated_at": None,
+    "fw_version": None
+}
+insert_device(device)
+devices = list_devices()
+print(devices)  # Debería mostrar la lista con el dispositivo insertado
+
